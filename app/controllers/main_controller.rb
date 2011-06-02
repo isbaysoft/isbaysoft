a942cc4a01a286a6422f77d22e5de8ef18c2cb82
Andrew <mail@isbaysoft.com>
2011-06-02 17:58:14 +0300

    work 20110602

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -29,6 +29,7 @@ class MainController < MainApplicationController
     @document = Document.find_by_id(params[:id]);
     @document_menu = Menu.permitted.find_by_id(@document.menu_id) unless @document.menu.nil?
     @document_menu_items = MenuItem.permitted.published.items(@document_menu.id) unless @document_menu.nil?
+
 #  rescue
 #    render 'errors/filenotfound'
   end
