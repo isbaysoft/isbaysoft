e17111c42542b118ba3fb7988c8a227d151a9134
isbaysoft <isbaysoft@gmail.com>
2011-12-15 00:12:20 +0200

    con

--- app/models/regnotification.rb
+++ app/models/regnotification.rb
@@ -5,46 +5,36 @@ class Regnotification < ActionMailer::Base
   self.sitename = Admin::ConfigHelper.sitename or 'site.com'
   self.admin_email = Admin::ConfigHelper.adminemail or 'admin@site.com'
 
+  default :from => self.sitename, :content_type => 'text/html'
+
   def event_new_user(user)
-    recipients self.admin_email
-    from self.sitename
-    subject "На сайте зарегистрировался новый пользователь"
-    content_type 'text/html'
-    body :user => user
+    @user = user
+    mail(:to => self.admin_email, :subject => "На сайте зарегистрировался новый пользователь")
   end
 
   def event_hello(user,type)
-    recipients user.email
-    from self.sitename
-    subject "Регистрация на сайте #{@site_name}"
-    content_type 'text/html'
-    body :user => user
+    @user = user
     case type
     when 'type_auto'
       template 'event_hello_auto'
     else
       template 'event_hello_manual'
     end
+    mail(:to => user.email, :subject => "Регистрация на сайте #{@site_name}")
   end
 
   def user_activated(user)
-    recipients user.email
-    from self.sitename
-    subject "Регистрация на сайте #{@site_name}"
-    content_type 'text/html'
-    body  :header => Memo.get_memo('activate_mail_header'),
-          :body => Memo.get_memo('activate_mail_body'),
-          :footer => Memo.get_memo('activate_mail_footer')
+    @header => Memo.get_memo('activate_mail_header'),
+    @body => Memo.get_memo('activate_mail_body'),
+    @footer => Memo.get_memo('activate_mail_footer')
+    mail(:to => user.email, :subject => "Регистрация на сайте #{@site_name}")
   end
 
   def user_deactivated(user)
-    recipients user.email
-    from self.sitename
-    subject "Регистрация на сайте #{@site_name}"
-    content_type 'text/html'
-    body  :header => Memo.get_memo('deactivate_mail_header'),
-          :body => Memo.get_memo('deactivate_mail_body'),
-          :footer => Memo.get_memo('deactivate_mail_footer')
+    @header => Memo.get_memo('deactivate_mail_header'),
+    @body => Memo.get_memo('deactivate_mail_body'),
+    @footer => Memo.get_memo('deactivate_mail_footer')
+    mail(:to => user.email, :subject "Регистрация на сайте #{@site_name}")
   end
   
 
