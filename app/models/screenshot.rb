56a8dc0d243348df4802fe78de3497ae0831c8d1
Andrew Bondarets <isbaysoft@gmail.com>
2012-04-12 18:54:43 +0300

    screenshots upload in progress

--- app/models/screenshot.rb
+++ app/models/screenshot.rb
@@ -5,7 +5,7 @@ class Screenshot < ActiveRecord::Base
     :path => ':rails_root/public/screenshots/:class/:document_id/:basename.:extension',
     :url => '/screenshots/:class/:document_id/:basename.:extension',
     :default_style => :thumb,
-    :styles => {:thumb => "128x128#"}
+    :styles => {:thumb => "160x120#"}
 
   validates_attachment_content_type :screenshot, :content_type => /image/
   validates_attachment_size :screenshot, :less_than => 2.megabyte,
